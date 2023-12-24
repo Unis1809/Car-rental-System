4f58d3a407e529bd572b5f4afa248741a34c3ef8
Nour Mohd <mohdnourxalgamed@gmail.com>
2023-12-25 00:52:05 +0200

    tazbeeet

--- index.js
+++ index.js
@@ -7,20 +7,22 @@ import session from "express-session";
 
 const app = express();
 const PORT = 5000;
+// var customer;
+// var car;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-    secret: 'your-secret-key',
-    resave: true,
-    saveUninitialized: true
+    secret: '123456789nnn',
+    resave: false,
+    saveUninitialized: false
 }));
 
 // Database connection
 const connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root', 
-    password: '', 
+    user: 'root',
+    password: '',
     database: 'cars_system',
 });
 
@@ -47,6 +49,7 @@ app.post('/login', (req, res) => {
         'SELECT * FROM customers WHERE Email = ?',
         [email],
         (err, users) => {
+
             if (err) {
                 res.status(500).send('Error during login');
                 return;
@@ -57,29 +60,17 @@ app.post('/login', (req, res) => {
                 res.render('login.ejs', { error: 'Invalid email or password' });
                 return;
             }
-            let customerId = null;
 
-            var flagCheck = false
-            users.forEach((user) => {
-                const isFound = bcrypt.compareSync(password, user.pass);
+            const user = users[0];
+            const passCheck = bcrypt.compareSync(password, user.pass);
 
-                if (isFound){
-                    flagCheck = true
-                    customerId = user.CustomerID;
-                    req.session.customer = {
-                        CustomerID: customerId,
-                    
-                    };
-
-                    // req.session.user = user;
-                    res.redirect('/dashboard');
-                    return;
-                }
-            });
-
-        if(!flagCheck) {
-        res.status(401).send('Invalid username or password');
-        }
+            if (passCheck) {
+                req.session.customer = user;
+                res.redirect('/dashboard');
+                return;
+            } else {
+                res.status(401).send('Invalid username or password');
+            }
 
         });
 
@@ -117,122 +108,61 @@ app.post('/register', (req, res) => {  // Needed : check for email or pass valid
 
 
 app.get('/reservation', (req, res) => {
-    const customer = req.session.customer;
-    const selectedCarId = req.query.selectedCarID;
-    const reservationDate = new Date().toLocaleDateString(); // Assuming you want to set it to the current date
-    
-    if (!selectedCarId) {
-        res.status(400).send('No car selected for reservation');
-        return;
-    }
+    const selectedCarId = req.query.CarID;
+    const reservationDate = new Date().toLocaleDateString(); // Assuming it to the current date
 
-    const customerId = req.session.customer.CustomerID;
-
-    // Fetch customer details using the customerId from the session
+    // Fetch car details 
     connection.query(
-        'SELECT * FROM customers WHERE CustomerID = ?',
-        [customerId],
-        (err, results) => {
-            if (err || results.length === 0) {
-                console.error('Error fetching customer details:', err);
-                res.status(500).send('Error fetching customer details');
+        'SELECT * FROM cars WHERE CarID = ?',
+        [selectedCarId],
+        (err, selectedCar) => {
+            const car = selectedCar[0];
+            if (err || selectedCar.length === 0) {
+                console.error('Error fetching car details:', err);
+                res.status(500).send('Error fetching car details');
                 return;
             }
-
-            // Store the fetched customer details in the session
-            req.session.customerDetails = results[0];
-            
-            // Fetch car details using selectedCarId
-            connection.query(
-                'SELECT * FROM cars WHERE CarID = ?',
-                [selectedCarId],
-                (err, results) => {
-                    if (err || results.length === 0) {
-                        console.error('Error fetching car details:', err);
-                        res.status(500).send('Error fetching car details');
-                        return;
-                    }
-
-                    // Store the fetched car details in the session
-                    req.session.selectedCar = results[0];
-
-                    // Render the reservation page with the selected car details, customerDetails, and reservationDate
-                    res.render('reservation.ejs', { 
-                        customerDetails: req.session.customerDetails, 
-                        selectedCar: req.session.selectedCar,
-                        reservationDate: reservationDate
-                    });
-                }
-            );
-        }
-    );
-});
-
-
-
-
-app.get('/dashboard', (req, res) => {
-    connection.query(
-        'SELECT * FROM cars',
-        (err, cars) =>{
-            if (err) {
-                res.status(500).send('Error during login');
-                return;
-            }
+            req.session.car = car;
+            const customer = req.session.customer;
             const renders = {
-                carsList : cars,
-                error : "No available cars"
-            } 
-            res.render('welcome.ejs', renders);
-        }
-    )
-    
+                customerDetails: customer,
+                selectedCar: car,
+                reservationDate: reservationDate
+            }
+            res.render('reservation.ejs', renders);
 
-    // if (req.session.user) {
-    //     const renders = {
-    //         firstName: req.session.user.FirstName,
-    //         lastName: req.session.user.LastName
-    //     }
-    //     res.render('welcome.ejs', renders);
-    // } else {
-    //     res.redirect('/');
-    // }
+        });
 });
+
+
+
+
 
 app.post('/reservation', (req, res) => {
-    const selectedCarId = req.body.selectedCarID;
-    const { pickupDate, returnDate } = req.body;
-    
-    // Directly use the selectedCar from the session
-    const selectedCar = req.session.selectedCar;
-    if (!selectedCar) {
-        res.status(400).send('No car selected for reservation');
-        return;
-    }
+    const selectedCustomerId = req.session.customer.CustomerID;
+    const selectedCarId = req.session.car.CarID;
+    const pickupDate = req.body.pickupDate;
+    const returnDate = req.body.returnDate;
+    const status = 'reserved';
 
-    const customerId = req.session.customerDetails.CustomerID;  // Assuming you have the customer's ID in the session
-    let unitprice = selectedCar.unitprice;  // Using directly from the session
 
-    // Calculate the total price
-    const pickupDateObj = new Date(pickupDate);
-    const returnDateObj = new Date(returnDate);
-    const totalPrice = (returnDateObj - pickupDateObj) / (1000 * 60 * 60 * 24) * unitprice;
 
-    // Save the reservation to the database
+
+    // // Save the reservation to the database
     connection.query(
         'INSERT INTO Reservations (CustomerID, CarID, ReservationDate, PickupDate, ReturnDate, Status) VALUES (?, ?, CURDATE(), ?, ?, ?)',
-        [customerId, selectedCarId, pickupDate, returnDate, 'reserved', totalPrice],
+        [selectedCustomerId, selectedCarId, pickupDate, returnDate, status],
         (err, results) => {
             if (err) {
                 console.error('Error reserving car:', err);
                 res.status(500).send('Error reserving car');
                 return;
             }
-            
+
             // Update the car's status to 'reserved'
             connection.query(
                 'UPDATE cars SET Status = ? WHERE CarID = ?',
-                ['reserved', selectedCarId],
+                [status, selectedCarId],
                 (err) => {
                     if (err) {
                         console.error('Error updating car status:', err);
@@ -241,10 +171,34 @@ app.post('/reservation', (req, res) => {
                     }
                     // Redirect to dashboard after successful reservation and status update
                     res.redirect('/dashboard');
-                }
-            );
-        }
-    );
+                });
+        });
+});
+
+app.get('/dashboard', (req, res) => {
+    const customer = req.session.customer;
+    connection.query(
+        'UPDATE cars SET Status = "Active" WHERE CarID IN (SELECT CarID FROM Reservations WHERE CURDATE() > ReturnDate AND Status = "reserved")',
+        (err) => {
+            if (err) {
+                console.error('Error updating car statuses:', err);
+            }
+
+            connection.query(
+                'SELECT * FROM cars',
+                (err, cars) => {
+                    if (err) {
+                        res.status(500).send('Error during login');
+                        return;
+                    }
+                    const renders = {
+                        customerDetails: customer,
+                        carsList: cars,
+                        error: "No available cars"
+                    }
+                    res.render('welcome.ejs', renders);
+                });
+        });
 });
 
 
